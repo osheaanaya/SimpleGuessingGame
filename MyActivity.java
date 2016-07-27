61d30ae34b9f496b8829730fd89c7c2369b2167d
osheaanaya <osheaanaya@live.com>
2016-07-26 23:22:54 -0700

    Update MyActivity.java

--- MyActivity.java
+++ MyActivity.java
@@ -1,10 +1,8 @@
 package com.example.oshea.myfirstapp;
 
 
-//import android.Manifest;
 import android.content.Context;
 import android.os.Bundle;
-//import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Menu;
